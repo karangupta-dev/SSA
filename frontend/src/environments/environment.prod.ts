dc9ab3ff32ae0f686b1f0be2ad48acd43b4bfab7
Karan Gupta <karangupta.dev@gmail.com>
2020-08-13 20:30:46 +0530

    Updated credentials

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,11 +1,16 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyABTrp20RlMaPUDUCse_7nWDLxq64TaHUg',
-    authDomain: 'socialspace-d1591.firebaseapp.com',
-    databaseURL: 'https://socialspace-d1591.firebaseio.com/',
-    projectId: 'socialspace-d1591',
-    storageBucket: 'socialspace-d1591.appspot.com',
+    // apiKey: 'AIzaSyABTrp20RlMaPUDUCse_7nWDLxq64TaHUg',
+    apiKey: 'AIzaSyCaXHusCocTHDlBAbZLUs-AfIutIP9QETU',
+    // authDomain: 'socialspace-d1591.firebaseapp.com',
+    authDomain: 'ssa2-f6010.firebaseapp.com',
+    // databaseURL: 'https://socialspace-d1591.firebaseio.com/',
+    databaseURL: 'https://ssa2-f6010.firebaseio.com/',
+    // projectId: 'socialspace-d1591',
+    projectId: 'ssa2-f6010',
+    // storageBucket: 'socialspace-d1591.appspot.com',
+    storageBucket: 'ssa2-f6010.appspot.com',
     messagingSenderId: '763399536402'
   }
 };
